9c4e35258304e8533be1ba4b5fc7c615c9f8b7fe
Nathan Pratta Teodosio <nathan.teodosio@canonical.com>
2023-12-05 10:20:27 +0100

    Hello world.

---
+++ hello.go
@@ -0,0 +1,22 @@
+package main
+//import "fmt"
+
+func TransHelloWorld(lang string) string {
+    helloWorlds := map[string]string {
+        "en": "Hello, world.",
+        "de": "Hallo, Welt.",
+        "es": "Hola, mundo.",
+        "lt": "Salve, orbis.",
+    }
+
+    if s, ok := helloWorlds[lang]; ok {
+        return s
+    }
+    return helloWorlds["en"]
+}
+
+//func main() {
+//    fmt.Println(TransHelloWorld("lt"))
+//    fmt.Println(TransHelloWorld("en"))
+//    fmt.Println(TransHelloWorld("no"))
+//}
