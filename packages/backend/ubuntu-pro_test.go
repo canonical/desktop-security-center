5e5bc045e28f6fa246d91dc7bcc56d3a3f138df0
Nathan Pratta Teodosio <nathan.teodosio@canonical.com>
2024-02-19 16:22:57 +0100

    Format and structure

--- packages/backend/ubuntu-pro_test.go
+++ packages/backend/ubuntu-pro_test.go
@@ -1,148 +1,159 @@
 package main
 
 import (
-    "testing"
-    "context"
-    //epb "google.golang.org/protobuf/types/known/emptypb"
-    "github.com/canonical/desktop-security-center/packages/testtools/testutils"
-    "fmt"
-    "os"
-    "github.com/godbus/dbus/v5"
+	"context"
+	"testing"
+	//epb "google.golang.org/protobuf/types/known/emptypb"
+	"fmt"
+	pb "github.com/canonical/desktop-security-center/packages/proto"
+	"github.com/canonical/desktop-security-center/packages/testtools/testutils"
+	"github.com/godbus/dbus/v5"
 	"github.com/godbus/dbus/v5/introspect"
-    pb "github.com/canonical/desktop-security-center/packages/proto"
+	"os"
 )
 
-var ret string
+type prodbus struct{}
+
+var getResponse string
+var manager *Manager
+var ctx context.Context
 
 func TestAttachProToMachine(t *testing.T) {
 	t.Cleanup(testutils.StartLocalSystemBus())
 
-    ctx := context.Background()
-    conn := testutils.NewDbusConn(t)
+	req := new(pb.AttachRequest)
+	req.Token = "incorrect"
+	_, err := manager.proServer.AttachProToMachine(ctx, req)
+	if err == nil {
+		t.Fatal("I expected error for incorrect token, but didn't get one.")
+	}
 
-    manager, err := NewServerManager(ctx, conn)
-    if err != nil {
-        t.Fatal("Couldn't get a manager", err)
-    }
+	req.Token = "correct"
+	_, err = manager.proServer.AttachProToMachine(ctx, req)
+	if err != nil {
+		t.Fatal("I expected no error for a correct token, got ", err)
+	}
+}
 
-    req := new(pb.AttachRequest)
-    req.Token = "incorrect"
-    _, err = manager.proServer.AttachProToMachine(ctx, req)
-    if err == nil {
-         t.Fatal("I expected error for incorrect token, but didn't get one.")
-    }
-    
-    req.Token = "correct"
-    _, err = manager.proServer.AttachProToMachine(ctx, req)
-    if err != nil {
-         t.Fatal("I expected no error for a correct token, got ", err)
-    }
+func TestIsMachineProAttached(t *testing.T) {
+	getResponse = "true"
+	i, err := manager.proServer.IsMachineProAttached(ctx, nil)
+	if i.GetValue() != true || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
 
-    /* IsMachineProAttached */
-    ret = "true"
-    i, err := manager.proServer.IsMachineProAttached(ctx, nil)
-    if i.GetValue() != true || err != nil {
-        t.Fatal("")
-    }
+	getResponse = "false"
+	i, err = manager.proServer.IsMachineProAttached(ctx, nil)
+	if i.GetValue() != false || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
 
-    ret = "false"
-    i, err = manager.proServer.IsMachineProAttached(ctx, nil)
-    if i.GetValue() != false || err != nil {
-        t.Fatal("")
-    }
-    /* Hangs, why though? 
-    fmt.Println(3)
-    ret = "error"
-    _, err = manager.proServer.IsMachineProAttached(ctx, nil)
-    fmt.Println(4)
-    if err != nil{
-         t.Fatal("I expected error but got none")
-    }
-    */
-    
-    /* IsServiceEnabled */
-    ret = "disabled"
-    i, err = manager.proServer.IsEsmInfraEnabled(ctx, nil)
-    if i.GetValue() != false || err != nil {
-        t.Fatal(i.GetValue(), err)
-    }
-    i, err = manager.proServer.IsEsmAppsEnabled(ctx, nil)
-    if i.GetValue() != false || err != nil {
-        t.Fatal(i.GetValue(), err)
-    }
-    i, err = manager.proServer.IsKernelLivePatchEnabled(ctx, nil)
-    if i.GetValue() != false || err != nil {
-        t.Fatal(i.GetValue(), err)
-    }
+	getResponse = "error"
+	_, err = manager.proServer.IsMachineProAttached(ctx, nil)
+	if err == nil {
+		t.Fatal("I expected an error, but didn't get one")
+	}
+}
 
-    ret = "enabled"
-    i, err = manager.proServer.IsEsmInfraEnabled(ctx, nil)
-    if i.GetValue() != true || err != nil {
-        t.Fatal()
-    }
-    i, err = manager.proServer.IsEsmAppsEnabled(ctx, nil)
-    if i.GetValue() != true || err != nil {
-        t.Fatal()
-    }
-    i, err = manager.proServer.IsKernelLivePatchEnabled(ctx, nil)
-    if i.GetValue() != true || err != nil {
-        t.Fatal()
-    }
-    /* Hangs, why though?
-    ret = "error"
-    i, err = manager.proServer.IsEsmInfraEnabled(ctx, nil)
-    if err != nil {
-        t.Fatal("Ni;")
-    }
-    */
+func TestIsServiceEnabled(t *testing.T) {
+	getResponse = "disabled"
+	i, err := manager.proServer.IsEsmInfraEnabled(ctx, nil)
+	if i.GetValue() != false || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
+	i, err = manager.proServer.IsEsmAppsEnabled(ctx, nil)
+	if i.GetValue() != false || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
+	i, err = manager.proServer.IsKernelLivePatchEnabled(ctx, nil)
+	if i.GetValue() != false || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
+
+	getResponse = "enabled"
+	i, err = manager.proServer.IsEsmInfraEnabled(ctx, nil)
+	if i.GetValue() != true || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
+	i, err = manager.proServer.IsEsmAppsEnabled(ctx, nil)
+	if i.GetValue() != true || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
+	i, err = manager.proServer.IsKernelLivePatchEnabled(ctx, nil)
+	if i.GetValue() != true || err != nil {
+		t.Fatal(i.GetValue(), err)
+	}
+
+	getResponse = "error"
+	i, err = manager.proServer.IsEsmInfraEnabled(ctx, nil)
+	if err == nil {
+		t.Fatal("I expected no error, but didn't get one")
+	}
+	i, err = manager.proServer.IsEsmAppsEnabled(ctx, nil)
+	if err == nil {
+		t.Fatal("I expected no error, but didn't get one")
+	}
+	i, err = manager.proServer.IsKernelLivePatchEnabled(ctx, nil)
+	if err == nil {
+		t.Fatal("I expected no error, but didn't get one")
+	}
 }
 
 func (a prodbus) Get(interfaceName string, propertyName string) (interface{}, *dbus.Error) {
-  defer fmt.Println("deferred", ret)
-  switch ret {
-  case "true": return true, nil
-  case "false": return false, nil
-  case "enabled": return "enabled", nil
-  case "disabled": return "disabled", nil
-  }
-  return "", dbus.NewError("failed", []interface{}{"fooerror"})
+	switch getResponse {
+	case "true":
+		return true, nil
+	case "false":
+		return false, nil
+	case "enabled":
+		return "enabled", nil
+	case "disabled":
+		return "disabled", nil
+	}
+	return "", dbus.NewError(
+		"com.canonical.UbuntuAdvantage.Error.GetError",
+		[]interface{}{"error requested in Get mocked method."},
+	)
 }
-func TestMain(m *testing.M){
-    defer testutils.StartLocalSystemBus()()
-    //For debugging with D-Feet
-    fmt.Println(os.Getenv("DBUS_SYSTEM_BUS_ADDRESS"))
+
+func TestMain(m *testing.M) {
+	defer testutils.StartLocalSystemBus()()
+	//For debugging with D-Feet
+	fmt.Println(os.Getenv("DBUS_SYSTEM_BUS_ADDRESS"))
 
 	conn, err := testutils.GetSystemBusConnection()
 	if err != nil {
-        fmt.Println(err)
+		fmt.Println("Couldn't connect to bus: ", err)
 		os.Exit(7)
 	}
 
-    if er := ExportAttachMock(conn); er != nil {
-        fmt.Println(er)
-        os.Exit(8)
-    }
-    m.Run()
+	if err = ExportAttachMock(conn); err != nil {
+		fmt.Println("Couldn't export mocks:", err)
+		os.Exit(8)
+	}
+
+	ctx = context.Background()
+	manager, err = NewServerManager(ctx, conn)
+	if err != nil {
+		fmt.Println("Couldn't get a manager: ", err)
+		os.Exit(9)
+	}
+	m.Run()
 }
 
-/*Mocking, consult https://github.com/canonical/ubuntu-desktop-provision/blob/main/provd/internal/testutils/accounts_mock.go#L61 for example*/
-type prodbus struct{}
-
-
-func (a prodbus) Ping() *dbus.Error {
-	return nil
-}
-
-func (a prodbus) Attach(token string) (*dbus.Error) {
+func (a prodbus) Attach(token string) *dbus.Error {
 	if token != "correct" {
-		return dbus.NewError("com.canonical.UbuntuAdvantage.Error.AttachError", []interface{}{"error requested in AttachError mocked method. Argument: " + token})
+		return dbus.NewError(
+			"com.canonical.UbuntuAdvantage.Error.AttachError",
+			[]interface{}{"error requested in AttachError mocked method."},
+		)
 	}
 	return nil
 }
 
 func ExportAttachMock(conn *dbus.Conn) error {
-    a := prodbus{}
-    servicesMock:=fmt.Sprintf(`
+	a := prodbus{}
+	servicesMock := fmt.Sprintf(`
     <node name="/">
       <interface name='com.canonical.UbuntuAdvantage.Service'>
         <method name='Enable'/>
@@ -150,7 +161,7 @@ func ExportAttachMock(conn *dbus.Conn) error {
       </interface>
     %s</node>
     `, introspect.IntrospectDataString)
-    mock:=fmt.Sprintf(`
+	managerMock := fmt.Sprintf(`
     <node name="/">
       <interface name='com.canonical.UbuntuAdvantage.Manager'>
         <method name='Attach'>
@@ -169,53 +180,39 @@ func ExportAttachMock(conn *dbus.Conn) error {
       </interface>
     %s</node>
     `, introspect.IntrospectDataString)
-    if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Manager"), "com.canonical.UbuntuAdvantage.Manager"); err != nil {
-        return fmt.Errorf("could not export  mock: %w", err)
-    }
-    if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/esm_2dapps"), "com.canonical.UbuntuAdvantage.Service"); err != nil {
-        return fmt.Errorf("could not export  mock: %w", err)
-    }
-    if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/esm_2dinfra"), "com.canonical.UbuntuAdvantage.Service"); err != nil {
-        return fmt.Errorf("could not export  mock: %w", err)
-    }
-    if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/livepatch"), "com.canonical.UbuntuAdvantage.Service"); err != nil {
-        return fmt.Errorf("could not export  mock: %w", err)
-    }
+	for _, val := range []string{
+		"esm_2dapps",
+		"esm_2dinfra",
+		"livepatch",
+	} {
+		if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/"+val), "com.canonical.UbuntuAdvantage.Service"); err != nil {
+			return fmt.Errorf("could not export service mock: %w", err)
+		}
+		if err := conn.Export(introspect.Introspectable(servicesMock), dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/"+val), "org.freedesktop.DBus.Introspectable"); err != nil {
+			return fmt.Errorf("could not export introspectable for service mock: %w", err)
+		}
+		if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/"+val), "org.freedesktop.DBus.Properties"); err != nil {
+			return fmt.Errorf("could not export Dbus properties mock: %w", err)
+		}
+	}
 
-    if err := conn.Export(introspect.Introspectable(mock), dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Manager"), "org.freedesktop.DBus.Introspectable"); err != nil {
-        return fmt.Errorf("could not export introspectable for accounts mock: %w", err)
-    }
+	if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Manager"), "com.canonical.UbuntuAdvantage.Manager"); err != nil {
+		return fmt.Errorf("could not export manager mock: %w", err)
+	}
+	if err := conn.Export(introspect.Introspectable(managerMock), dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Manager"), "org.freedesktop.DBus.Introspectable"); err != nil {
+		return fmt.Errorf("could not export introspectable for manager mock: %w", err)
+	}
 	if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Manager"), "org.freedesktop.DBus.Properties"); err != nil {
-		return fmt.Errorf("could not export DBUS PROPERTIES mock: %w", err)
-	}
-
-    if err := conn.Export(introspect.Introspectable(servicesMock), dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/livepatch"), "org.freedesktop.DBus.Introspectable"); err != nil {
-        return fmt.Errorf("could not export introspectable for accounts mock: %w", err)
-    }
-	if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/livepatch"), "org.freedesktop.DBus.Properties"); err != nil {
-		return fmt.Errorf("could not export DBUS PROPERTIES mock: %w", err)
-	}
-    if err := conn.Export(introspect.Introspectable(servicesMock), dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/esm_2dinfra"), "org.freedesktop.DBus.Introspectable"); err != nil {
-        return fmt.Errorf("could not export introspectable for accounts mock: %w", err)
-    }
-	if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/esm_2dinfra"), "org.freedesktop.DBus.Properties"); err != nil {
-		return fmt.Errorf("could not export DBUS PROPERTIES mock: %w", err)
-	}
-    if err := conn.Export(introspect.Introspectable(servicesMock), dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/esm_2dapps"), "org.freedesktop.DBus.Introspectable"); err != nil {
-        return fmt.Errorf("could not export introspectable for accounts mock: %w", err)
-    }
-	if err := conn.Export(a, dbus.ObjectPath("/com/canonical/UbuntuAdvantage/Services/esm_2dapps"), "org.freedesktop.DBus.Properties"); err != nil {
-		return fmt.Errorf("could not export DBUS PROPERTIES mock: %w", err)
+		return fmt.Errorf("could not export Dbus properties mock: %w", err)
 	}
 
 	reply, err := conn.RequestName("com.canonical.UbuntuAdvantage", dbus.NameFlagDoNotQueue)
 	if err != nil {
-		return fmt.Errorf("failed to acquire account name on local system bus: %w", err)
+		return fmt.Errorf("failed to acquire Ubuntu Advantage name on local system bus: %w", err)
 	}
 	if reply != dbus.RequestNameReplyPrimaryOwner {
-		return fmt.Errorf("failed to acquire account name on local system bus: name is already taken")
+		return fmt.Errorf("failed to acquire Ubuntu Advantage name on local system bus: name is already taken")
 	}
 
-    return nil
-    
+	return nil
 }
