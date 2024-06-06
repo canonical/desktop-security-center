0faf455828efa642d24db0d5fc99cc4c15ff1a0d
Nathan Pratta Teodosio <nathan.teodosio@canonical.com>
2024-06-06 15:07:37 +0200

    Move Json constants to other file.

---
+++ packages/backend/json-consts.go
@@ -0,0 +1,127 @@
+package main
+
+const (
+appPermissionsJson = `{
+  "type": "sync",
+  "status-code": 200,
+  "status": "OK",
+  "result":
+    {
+      "experimental":
+        {
+          "apparmor-prompting": %v
+        }
+    }
+}`
+
+enablePermissionsJson = `{
+  "type": "async",
+  "status-code": 202,
+  "status": "Accepted",
+  "result": null,
+  "change": "92"
+}`
+
+queryPermissionsEnabledJson = `{
+  "type": "sync",
+  "status-code": 200,
+  "status": "OK",
+  "result": {
+    "experimental": {
+      "apparmor-prompting": true
+    },
+    "refresh": {},
+    "seed": {
+      "loaded": true
+    },
+    "system": {
+      "hostname": "prompting",
+      "network": {},
+      "timezone": "UTC"
+    }
+  }
+}`
+
+customRulesJson = `{
+  "type": "sync",
+  "status-code": 200,
+  "status": "OK",
+  "result": [
+    {
+      "id": "C7JGESQZTWTSS===",
+      "timestamp": "2024-05-24T09:21:18.378444585Z",
+      "user": 1000,
+      "snap": "simple-notepad",
+      "interface": "home",
+      "constraints": {
+        "path-pattern": "/home/ubuntu/.config/fobar",
+        "permissions": [
+          "read",
+          "write"
+        ]
+      },
+      "outcome": "allow",
+      "lifespan": "forever",
+      "expiration": "0001-01-01T00:00:00Z"
+    },
+    {
+      "id": "C7JHBW7E7Q7PO===",
+      "timestamp": "2024-05-24T13:48:17.723465463Z",
+      "user": 1000,
+      "snap": "simple-notepad",
+      "interface": "home",
+      "constraints": {
+        "path-pattern": "/home/ubuntu/Documents/fobar",
+        "permissions": [
+          "read",
+          "write"
+        ]
+      },
+      "outcome": "allow",
+      "lifespan": "forever",
+      "expiration": "0001-01-01T00:00:00Z"
+    }
+  ]
+}`
+
+noCustomRulesJson = `{
+  "type": "sync",
+  "status-code": 200,
+  "status": "OK",
+  "result": []
+}`
+
+idNotFoundJson = `{
+  "type": "error",
+  "status-code": 500,
+  "status": "Internal Server Error",
+  "result": {
+    "message": "rule ID is not found"
+  }
+}`
+
+idRemoved = `{
+  "type": "sync",
+  "status-code": 200,
+  "status": "OK",
+  "result": [
+    {
+      "id": "C7JHBW7E7Q7PO===",
+      "timestamp": "2024-05-24T13:48:17.723465463Z",
+      "user": 1000,
+      "snap": "simple-notepad",
+      "interface": "home",
+      "constraints": {
+        "path-pattern": "/home/ubuntu/Documents/fobar",
+        "permissions": [
+          "read",
+          "write"
+        ]
+      },
+      "outcome": "allow",
+      "lifespan": "forever",
+      "expiration": "0001-01-01T00:00:00Z"
+    }
+  ]
+}`
+)
--- packages/backend/permissions_test.go
+++ packages/backend/permissions_test.go
@@ -11,61 +11,16 @@ import (
     pb "github.com/canonical/desktop-security-center/packages/proto"
 )
 
-const (
-    appPermissionsJson = `
-{
-  "type": "sync",
-  "status-code": 200,
-  "status": "OK",
-  "result":
-    {
-      "experimental":
-        {
-          "apparmor-prompting": %v
-        }
-    }
-}
-`
-enablePermissionsJson = `
-{"type":"async","status-code":202,"status":"Accepted","result":null,"change":"92"}
-`
-queryPermissionsEnabledJson = `
-{"type":"sync","status-code":200,"status":"OK","result":{"experimental":{"apparmor-prompting":true},"refresh":{},"seed":{"loaded":true},"system":{"hostname":"prompting","network":{},"timezone":"UTC"}}}
-`
-    customRulesJson = `
-{"type":"sync","status-code":200,"status":"OK","result":[{"id":"C7JGESQZTWTSS===","timestamp":"2024-05-24T09:21:18.378444585Z","user":1000,"snap":"simple-notepad","interface":"home","constraints":{"path-pattern":"/home/ubuntu/.config/fobar","permissions":["read","write"]},"outcome":"allow","lifespan":"forever","expiration":"0001-01-01T00:00:00Z"},{"id":"C7JHBW7E7Q7PO===","timestamp":"2024-05-24T13:48:17.723465463Z","user":1000,"snap":"simple-notepad","interface":"home","constraints":{"path-pattern":"/home/ubuntu/Documents/fobar","permissions":["read","write"]},"outcome":"allow","lifespan":"forever","expiration":"0001-01-01T00:00:00Z"}]}
-`
-    noCustomRulesJson = `
-{"type":"sync","status-code":200,"status":"OK","result":[]}
-`
-    idNotFoundJson = `
-{"type":"error","status-code":500,"status":"Internal Server Error","result":{"message":"rule ID is not found"}}
-`
-    idRemoved = `
-{"type":"sync","status-code":200,"status":"OK","result":[{"id":"C7JHBW7E7Q7PO===","timestamp":"2024-05-24T13:48:17.723465463Z","user":1000,"snap":"simple-notepad","interface":"home","constraints":{"path-pattern":"/home/ubuntu/Documents/fobar","permissions":["read","write"]},"outcome":"allow","lifespan":"forever","expiration":"0001-01-01T00:00:00Z"}]}
-`
-)
 
 var listOfPersonalFoldersPerm = map[string][]string {
     "/home/ubuntu/.config/fobar": []string{"simple-notepad"},
     "/home/ubuntu/Documents/fobar": []string{"simple-notepad"},
 }
 
-type Function int
-const (
-    DisableAppPermissions Function = iota
-    EnableAppPermissions
-    IsAppPermissionsEnabled
-    AreCustomRulesApplied
-    RemoveAppPermission
-    ListPersonalFoldersPermissions
-)
-
 type ClientMock struct {
     wantError bool
     wantApiError bool
     isEnabled bool
-    testedFun Function
 }
 func (c *ClientMock) Do(req *http.Request) (*http.Response, error) {
     if c.wantError {
@@ -115,21 +70,18 @@ func (c *ClientMock) Do(req *http.Request) (*http.Response, error) {
     panic("Not reached")
 }
 
-func testToggleAppPermissions(t *testing.T, f Function) {
+func testToggleAppPermissions(t *testing.T, toggleTo bool) {
     tt := []struct {
         name     string
         wantError  bool
-        testedFun Function
     }{
         {
             name: "valid response",
             wantError: false,
-            testedFun: f,
         },
         {
             name: "invalid response",
             wantError: true,
-            testedFun: f,
         },
     }
     for i := range tt {
@@ -138,13 +90,12 @@ func testToggleAppPermissions(t *testing.T, f Function) {
             t.Parallel()
             client := &ClientMock{
                 wantError: tc.wantError,
-                testedFun: tc.testedFun,
             }
             var err error
-            switch tc.testedFun {
-            case EnableAppPermissions:
+            switch toggleTo {
+            case true:
                 _, err = NewPermissionServer(client).EnableAppPermissions(ctx, nil)
-            case DisableAppPermissions:
+            case false:
                 _, err = NewPermissionServer(client).DisableAppPermissions(ctx, nil)
             }
             if tc.wantError {
@@ -157,10 +108,10 @@ func testToggleAppPermissions(t *testing.T, f Function) {
 }
 
 func TestEnableAppPermissions(t *testing.T) {
-    testToggleAppPermissions(t, EnableAppPermissions)
+    testToggleAppPermissions(t, true)
 }
 func TestDisableAppPermissions(t *testing.T) {
-    testToggleAppPermissions(t, DisableAppPermissions)
+    testToggleAppPermissions(t, false)
 }
 func TestIsAppPermissionsEnabled(t *testing.T) {
     tt := []struct {
@@ -189,7 +140,6 @@ func TestIsAppPermissionsEnabled(t *testing.T) {
             t.Parallel()
             client := &ClientMock{
                 wantError: tc.wantError,
-                testedFun: IsAppPermissionsEnabled,
                 isEnabled: tc.isEnabled,
             }
             r, err := NewPermissionServer(client).IsAppPermissionsEnabled(ctx, nil)
@@ -229,7 +179,6 @@ func TestAreCustomRulesApplied(t *testing.T) {
             t.Parallel()
             client := &ClientMock{
                 wantError: tc.wantError,
-                testedFun: AreCustomRulesApplied,
                 isEnabled: tc.isEnabled,
             }
             var err error
@@ -265,7 +214,6 @@ func TestListPersonalFoldersPermissions(t *testing.T) {
             t.Parallel()
             client := &ClientMock{
                 wantError: tc.wantError,
-                testedFun: ListPersonalFoldersPermissions,
                 isEnabled: tc.isEnabled,
             }
             var err error
@@ -318,7 +266,6 @@ func TestRemoveAppPermission(t *testing.T) {
             client := &ClientMock{
                 wantError: tc.wantError,
                 wantApiError: tc.wantApiError,
-                testedFun: RemoveAppPermission,
                 isEnabled: tc.isEnabled,
             }
             req := &pb.RemoveAppPermissionRequest{
