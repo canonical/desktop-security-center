e33ddff0121b238b25c3faf0468d3a707d131ef6
nteodosio <105225958+nteodosio@users.noreply.github.com>
2024-06-10 10:05:14 +0000

    Fix CI (#42)
    * Install protoc-gen-go{-grpc,} from Ubuntu repositories.
    * Run on the ci branch too.
    * Rename job to backend.
    * Run backend job on 24.04.
      Annoyingly ubuntu-latest still translates to 22.04...
    * CI: Use go generate.
      As it greatly simplfies ci.yaml.

---
+++ packages/generate.go
@@ -0,0 +1,7 @@
+package main
+
+//TODO: Watch https://github.com/protocolbuffers/protobuf for any changes on the experimental status of optional fields,
+// previously described on: https://github.com/protocolbuffers/protobuf/blob/main/docs/implementing_proto3_presence.md.
+//
+// Should it become default, remove the --experimental_allow_proto3_optional flag from the go generate command below.
+//go:generate sh -c "PATH=\"$PATH:`go env GOPATH`/bin\" protoc --proto_path=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./proto/*.proto --experimental_allow_proto3_optional"
