0abf475822f7578bee524bd1771af33f53414135
Dennis Loose <dennis.loose@canonical.com>
2024-07-01 17:15:58 +0200

    test(security_center): add basic widget test

---
+++ flutter_packages/security_center/test/rules/snap_rules_page_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:security_center/rules/snap_rules_page.dart';
+import 'package:security_center/services/snapd_rules_service.dart';
+
+import '../test_utils.dart';
+
+void main() {
+  testWidgets('display and delete rules', (tester) async {
+    final container = createContainer();
+    final mockRulesService = registerMockRulesService(
+      rules: [
+        SnapdRule(
+          id: 'ruleId',
+          timestamp: DateTime(2024),
+          snap: 'firefox',
+          interface: 'home',
+          constraints: const SnapdConstraints(
+            pathPattern: '/home/ubuntu/**',
+            permissions: ['read', 'write'],
+          ),
+          outcome: SnapdRequestOutcome.allow,
+          lifespan: SnapdRequestLifespan.forever,
+        ),
+      ],
+    );
+    await tester.pumpApp(
+      (_) => UncontrolledProviderScope(
+        container: container,
+        child: const SnapRulesPage(
+          snap: 'firefox',
+          interface: 'home',
+        ),
+      ),
+    );
+    await tester.pump();
+
+    expect(find.text('Rules for firefox'), findsOneWidget);
+
+    await tester.tap(find.text('ruleId'));
+    verify(mockRulesService.removeRule('ruleId')).called(1);
+
+    await tester.tap(find.text('Remove all'));
+    verify(mockRulesService.removeAllRules(snap: 'firefox', interface: 'home'))
+        .called(1);
+  });
+}
