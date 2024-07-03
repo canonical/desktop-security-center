981512a0563360ffebf5cad468904793f546da00
Dennis Loose <dennis.loose@canonical.com>
2024-07-03 18:09:27 +0200

    test(security_center): add basic integration test

---
+++ flutter_packages/security_center/integration_test/security_center_test.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:security_center/main.dart' as app;
+import 'package:snapd/snapd.dart';
+import 'package:ubuntu_service/ubuntu_service.dart';
+
+const testRulesPath = 'integration_test/assets/test_rules.json';
+
+void main() {
+  tearDown(resetAllServices);
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('display and delete rules', (tester) async {
+    await writeSnapdRules();
+
+    await app.main([]);
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('home'));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('firefox'));
+    await tester.pumpAndSettle();
+
+    final testRules = getTestRules(interface: 'home', snap: 'firefox');
+
+    for (final rule in testRules) {
+      expectRule(rule);
+    }
+
+    await expectSnapdRules(testRules);
+
+    await tester.tap(find.text('Remove all'));
+    await tester.pumpAndSettle();
+
+    await expectSnapdRules([]);
+  });
+}
+
+void expectRule(SnapdRuleMask rule) {
+  final tile = [
+    find.text('Path pattern: ${rule.constraints.pathPattern}'),
+    find.text('Permissions: ${rule.constraints.permissions?.join(', ')}'),
+    find.text('Outcome: ${rule.outcome.name}'),
+    find.text('Lifespan: ${rule.lifespan.name}'),
+  ]
+      .map(
+        (finder) => find.ancestor(of: finder, matching: find.byType(ListTile)),
+      )
+      .map((finder) => finder.evaluate().toSet())
+      .reduce((value, element) => value.intersection(element));
+  expect(tile.length, equals(1));
+}
+
+List<SnapdRuleMask> getTestRules({String? snap, String? interface}) {
+  return (jsonDecode(File(testRulesPath).readAsStringSync()) as List)
+      .map((e) => SnapdRuleMask.fromJson(e as Map<String, dynamic>))
+      .where(
+        (rule) =>
+            (snap == null || rule.snap == snap) &&
+            (interface == null || rule.interface == interface),
+      )
+      .toList();
+}
+
+Future<void> writeSnapdRules() async {
+  final ruleMasks = getTestRules();
+  final snapd = SnapdClient();
+  for (final rule in ruleMasks) {
+    await snapd.addRule(rule);
+  }
+  snapd.close();
+}
+
+Future<void> expectSnapdRules(List<SnapdRuleMask> rules) async {
+  final snapd = SnapdClient();
+  final actual = await snapd.getRules();
+  snapd.close();
+
+  if (rules.isEmpty) {
+    expect(actual, isEmpty);
+    return;
+  }
+
+  for (final rule in rules) {
+    expect(actual.where(rule.matches), hasLength(1));
+  }
+}
+
+extension on SnapdRuleMask {
+  bool matches(SnapdRule rule) {
+    return (snap == rule.snap) &&
+        (interface == rule.interface) &&
+        (constraints == rule.constraints) &&
+        (outcome == rule.outcome) &&
+        (lifespan == rule.lifespan);
+  }
+}
