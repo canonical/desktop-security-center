85ed9fc13707226b6c880354e7b851d859614c69
Dennis Loose <dennis.loose@canonical.com>
2024-07-11 14:20:00 +0200

    feat: allow enabling/disabling prompting in the UI

--- lib/app_permissions/interfaces_page.dart
+++ lib/app_permissions/interfaces_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:security_center/app_permissions/rules_providers.dart';
-import 'package:security_center/app_permissions/snapd_interface.dart';
 import 'package:security_center/l10n.dart';
 import 'package:security_center/navigator.dart';
 import 'package:security_center/widgets/scrollable_page.dart';
@@ -13,10 +12,9 @@ class InterfacesPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final model = ref.watch(interfaceSnapCountsProvider);
+    final model = ref.watch(promptingEnabledProvider);
     return model.when(
-      data: (interfaceSnapCounts) =>
-          _Body(interfaceSnapCounts: interfaceSnapCounts),
+      data: (promptingEnabled) => _Body(promptingEnabled: promptingEnabled),
       error: (error, _) => ErrorWidget(error),
       loading: () => const Center(child: YaruCircularProgressIndicator()),
     );
@@ -24,50 +22,84 @@ class InterfacesPage extends ConsumerWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body({required this.interfaceSnapCounts});
+  const _Body({required this.promptingEnabled});
 
-  final Map<SnapdInterface, int> interfaceSnapCounts;
+  final bool promptingEnabled;
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
-    final tiles = interfaceSnapCounts.entries
-        .map(
-          (interfaceSnapCount) => ListTile(
-            contentPadding: const EdgeInsets.all(16),
-            leading: Icon(interfaceSnapCount.key.icon, size: 48),
-            title: Text(interfaceSnapCount.key.localizedTitle(l10n)),
-            subtitle: Text(l10n.interfaceSnapCount(interfaceSnapCount.value)),
-            trailing: const Icon(YaruIcons.pan_end),
-            onTap: () => Navigator.of(context)
-                .pushSnapPermissions(interface: interfaceSnapCount.key),
-          ),
-        )
-        .toList();
     return ScrollablePage(
       children: [
-        YaruBorderContainer(
-          child: YaruSwitchListTile(
-            title: Row(
-              children: [
-                Flexible(child: Text(l10n.snapPermissionsEnableTitle)),
-                const SizedBox(width: 10),
-                YaruInfoBadge(
-                  title: Text(l10n.snapPermissionsExperimentalLabel),
-                  yaruInfoType: YaruInfoType.information,
-                ),
-              ],
-            ),
-            subtitle: Text(l10n.snapPermissionsEnableWarning),
-            value: true,
-            onChanged: (value) {},
-          ),
-        ),
+        _PromptingSwitch(promptingEnabled: promptingEnabled),
         const SizedBox(height: 16),
-        TileList(children: tiles),
-        const SizedBox(height: 16),
+        if (promptingEnabled) ...[
+          const _InterfaceList(),
+          const SizedBox(height: 16),
+        ],
         Text(l10n.snapPermissionsOtherDescription),
       ],
     );
+  }
+}
+
+class _PromptingSwitch extends ConsumerWidget {
+  const _PromptingSwitch({
+    required this.promptingEnabled,
+  });
+
+  final bool promptingEnabled;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final notifier = ref.read(promptingEnabledProvider.notifier);
+    final l10n = AppLocalizations.of(context);
+    return YaruBorderContainer(
+      child: YaruSwitchListTile(
+        title: Row(
+          children: [
+            Flexible(child: Text(l10n.snapPermissionsEnableTitle)),
+            const SizedBox(width: 10),
+            YaruInfoBadge(
+              title: Text(l10n.snapPermissionsExperimentalLabel),
+              yaruInfoType: YaruInfoType.information,
+            ),
+          ],
+        ),
+        subtitle: Text(l10n.snapPermissionsEnableWarning),
+        value: promptingEnabled,
+        onChanged: (value) => value ? notifier.enable() : notifier.disable(),
+      ),
+    );
+  }
+}
+
+class _InterfaceList extends ConsumerWidget {
+  const _InterfaceList();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(interfaceSnapCountsProvider).when(
+          data: (interfaceSnapCounts) {
+            final l10n = AppLocalizations.of(context);
+            final tiles = interfaceSnapCounts.entries
+                .map(
+                  (interfaceSnapCount) => ListTile(
+                    contentPadding: const EdgeInsets.all(16),
+                    leading: Icon(interfaceSnapCount.key.icon, size: 48),
+                    title: Text(interfaceSnapCount.key.localizedTitle(l10n)),
+                    subtitle:
+                        Text(l10n.interfaceSnapCount(interfaceSnapCount.value)),
+                    trailing: const Icon(YaruIcons.pan_end),
+                    onTap: () => Navigator.of(context)
+                        .pushSnapPermissions(interface: interfaceSnapCount.key),
+                  ),
+                )
+                .toList();
+            return TileList(children: tiles);
+          },
+          error: (error, _) => ErrorWidget(error),
+          loading: () => const Center(child: YaruCircularProgressIndicator()),
+        );
   }
 }
--- test/app_permissions/interfaces_page_test.dart
+++ test/app_permissions/interfaces_page_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:security_center/app_permissions/interfaces_page.dart';
 import 'package:security_center/app_permissions/snapd_interface.dart';
 import 'package:security_center/services/app_permissions_service.dart';
+import 'package:yaru/yaru.dart';
 
 import '../test_utils.dart';
 
@@ -29,7 +31,7 @@ void main() {
         child: const InterfacesPage(),
       ),
     );
-    await tester.pump();
+    await tester.pumpAndSettle();
 
     final homeInterfaceTile = find.ancestor(
       of: find.text(SnapdInterface.home.localizedTitle(tester.l10n)),
@@ -43,5 +45,26 @@ void main() {
       ),
       findsOneWidget,
     );
+  });
+
+  testWidgets('toggle prompting', (tester) async {
+    final container = createContainer();
+    final service = registerMockRulesService(enabled: false);
+    await tester.pumpApp(
+      (_) => UncontrolledProviderScope(
+        container: container,
+        child: const InterfacesPage(),
+      ),
+    );
+    await tester.pump();
+
+    final toggle = find.byType(YaruSwitchListTile);
+    expect(toggle, findsOneWidget);
+    expect(tester.widget<YaruSwitchListTile>(toggle).value, isFalse);
+
+    await tester.tap(toggle);
+    await tester.pump();
+
+    verify(service.enable()).called(1);
   });
 }
