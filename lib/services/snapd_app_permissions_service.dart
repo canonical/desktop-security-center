b58d00f5c9b1c09d85f6a5b66980627b0b162cfa
Dennis Loose <dennis.loose@canonical.com>
2024-07-11 14:06:20 +0200

    feat: add service methods to toggle prompting

--- lib/services/snapd_app_permissions_service.dart
+++ lib/services/snapd_app_permissions_service.dart
@@ -9,22 +9,45 @@ class SnapdAppPermissionsService implements AppPermissionsService {
   Future<List<SnapdRule>> getRules({
     String? snap,
     String? interface,
-  }) async =>
+  }) =>
       _client.getRules(
         snap: snap,
         interface: interface,
       );
 
   @override
-  Future<void> removeRule(String id) async {
-    await _client.removeRule(id);
-  }
+  Future<void> removeRule(String id) => _client.removeRule(id);
 
   @override
-  Future<void> removeAllRules({required String snap, String? interface}) async {
-    await _client.removeRules(
-      snap,
-      interface: interface,
-    );
-  }
+  Future<void> removeAllRules({
+    required String snap,
+    String? interface,
+  }) =>
+      _client.removeRules(
+        snap,
+        interface: interface,
+      );
+
+  @override
+  Future<bool> isEnabled() => _client.systemInfo().then(
+        (systemInfo) =>
+            systemInfo.features!['apparmor-prompting']!['enabled'] as bool,
+      );
+
+  @override
+  Future<void> enable() => _client.enablePrompting().then(_client.waitChange);
+
+  @override
+  Future<void> disable() => _client.disablePrompting().then(_client.waitChange);
+}
+
+extension on SnapdClient {
+  Future<void> waitChange(
+    String id, {
+    Duration interval = const Duration(milliseconds: 100),
+  }) =>
+      Stream.periodic(interval, (_) => getChange(id))
+          .asyncMap((response) async => response)
+          .distinct()
+          .firstWhere((change) => change.ready);
 }
