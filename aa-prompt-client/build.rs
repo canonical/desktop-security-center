e9da202df5de8033691b8eabc26878dc69aeb8aa
Matthew Hagemann <matt.hagemann@canonical.com>
2024-07-16 16:34:10 +0200

    fix: fixed client failing to compile protos in snap build

--- aa-prompt-client/build.rs
+++ aa-prompt-client/build.rs
@@ -1,12 +1,19 @@
+use std::env;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let current_dir = env::current_dir()?;
+
+    println!("Current directory: {:?}", current_dir);
+
     std::fs::create_dir_all("./src/protos")?;
 
-    let files = &["../apparmor-prompting.proto"];
+    let files = &["../protos/apparmor-prompting.proto"];
+    let include_dirs = &["../protos"];
 
     tonic_build::configure()
         .build_server(true)
         .out_dir("./src/protos")
-        .compile(files, &["../"])?;
+        .compile(files, include_dirs)?;
 
     Ok(())
 }
