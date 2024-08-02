b35ab6d911e59a140503c3007d426bbe667d55fa
Dennis Loose <dennis.loose@canonical.com>
2024-08-02 16:14:22 +0200

    test: use l10n strings in integration test

--- integration_test/security_center_test.dart
+++ integration_test/security_center_test.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:security_center/app_permissions/app_rules_page.dart';
 import 'package:security_center/app_permissions/snapd_interface.dart';
 import 'package:security_center/main.dart' as app;
 import 'package:snapd/snapd.dart';
@@ -33,24 +34,31 @@ void main() {
     final testRules = getTestRules(interface: 'home', snap: 'firefox');
 
     for (final rule in testRules) {
-      expectRule(rule);
+      expectRule(tester, rule);
     }
 
     await expectSnapdRules(testRules);
 
-    await tester.tap(find.text('Remove all'));
+    await tester.tap(find.text(tester.l10n.snapRulesRemoveAll));
     await tester.pumpAndSettle();
 
     await expectSnapdRules([]);
   });
 }
 
-void expectRule(SnapdRuleMask rule) {
+void expectRule(WidgetTester tester, SnapdRuleMask rule) {
   // This verifies that the specified rule is displayed in the UI by checking
   // that the widgets in the following list share a unique common ancestor.
   final tile = [
     find.text(rule.constraints.pathPattern!),
-    find.text(rule.constraints.permissions!.join(', ')),
+    find.text(
+      rule.constraints.permissions!
+          .map(
+            (permission) =>
+                Permission.fromString(permission).localize(tester.l10n),
+          )
+          .join(', '),
+    ),
   ]
       // List of sets of ancestors for each text widget.
       .map(
