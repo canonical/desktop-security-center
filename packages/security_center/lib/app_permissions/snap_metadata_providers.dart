4820a8f2751813696e8e242de2df31fb2c1c2868
ashuntu <ashton.nelson@canonical.com>
2025-04-17 17:43:05 -0500

    Remove deprecated Ref types

--- packages/security_center/lib/app_permissions/snap_metadata_providers.dart
+++ packages/security_center/lib/app_permissions/snap_metadata_providers.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:snapd/snapd.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
@@ -8,16 +9,15 @@ part 'snap_metadata_providers.g.dart';
 typedef LocalSnapData = List<Snap>;
 
 @Riverpod(keepAlive: true)
-String? snapIconUrl(SnapIconUrlRef ref, String snapName) =>
-    getService<LocalSnapData>()
-        .firstWhereOrNull((snap) => snap.name == snapName)
-        ?.media
-        .where((m) => m.type == 'icon')
-        .firstOrNull
-        ?.url;
+String? snapIconUrl(Ref ref, String snapName) => getService<LocalSnapData>()
+    .firstWhereOrNull((snap) => snap.name == snapName)
+    ?.media
+    .where((m) => m.type == 'icon')
+    .firstOrNull
+    ?.url;
 
 @Riverpod(keepAlive: true)
-String snapTitleOrName(SnapTitleOrNameRef ref, String snapName) =>
+String snapTitleOrName(Ref ref, String snapName) =>
     getService<LocalSnapData>()
         .firstWhereOrNull((snap) => snap.name == snapName)
         ?.title ??
