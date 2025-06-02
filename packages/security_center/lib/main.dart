994b001c3ff1b309f16542797372e0762dd6ae1e
Matthew Hagemann <matt.hagemann@canonical.com>
2025-06-02 10:06:41 +0200

    fix: fix main parsing, fix test_containers.json casing

--- packages/security_center/lib/main.dart
+++ packages/security_center/lib/main.dart
@@ -6,7 +6,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:security_center/app_permissions/snap_metadata_providers.dart';
 import 'package:security_center/security_center_app.dart';
 import 'package:security_center/services/app_permissions_service.dart';
+import 'package:security_center/services/disk_encryption_service.dart';
 import 'package:security_center/services/fake_app_permissions_service.dart';
+import 'package:security_center/services/fake_disk_encryption_service.dart';
 import 'package:security_center/services/snapd_app_permissions_service.dart';
 import 'package:security_center/services/snapd_service.dart';
 import 'package:ubuntu_logger/ubuntu_logger.dart';
@@ -17,16 +19,14 @@ Future<void> main(List<String> args) async {
   await YaruWindowTitleBar.ensureInitialized();
   Logger.setup(path: '');
 
-  final parser = ArgParser()
-    ..addFlag(
-      'dry-run',
-      help: 'Use a fake rules service instead of snapd',
-    )
-    ..addOption(
-      'test-rules',
-      help: 'Path to a JSON file containing test rules',
-      defaultsTo: 'integration_test/assets/test_rules.json',
-    );
+  final parser =
+      ArgParser()
+        ..addFlag('dry-run', help: 'Use a fake rules service instead of snapd')
+        ..addOption(
+          'test-rules',
+          help: 'Path to a JSON file containing test rules',
+          defaultsTo: 'integration_test/assets/test_rules.json',
+        );
 
   final ArgResults argResults;
   try {
@@ -38,21 +38,25 @@ Future<void> main(List<String> args) async {
 
   registerService<SnapdService>(SnapdService.new);
 
+  registerService<DiskEncryptionService>(
+    () => FakeDiskEncryptionService.fromFile(
+      'integration_test/assets/test_containers.json',
+    )
+  );
+
   final snapMetadata = await getService<SnapdService>().getSnaps();
   registerServiceInstance<LocalSnapData>(snapMetadata);
 
   if (argResults.flag('dry-run')) {
     registerService<AppPermissionsService>(
-      () => FakeAppPermissionsService.fromFile(
-        argResults['test-rules'] as String,
-      )..init(),
+      () =>
+          FakeAppPermissionsService.fromFile(argResults['test-rules'] as String)
+            ..init(),
       dispose: (service) => service.dispose(),
     );
   } else {
     registerService<AppPermissionsService>(
-      () => SnapdAppPermissionsService(
-        getService<SnapdService>(),
-      )..init(),
+      () => SnapdAppPermissionsService(getService<SnapdService>())..init(),
       dispose: (service) => service.dispose(),
     );
   }
