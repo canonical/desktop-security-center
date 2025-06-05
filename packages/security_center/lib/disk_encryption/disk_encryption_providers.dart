3098692587a5355c459f9f26d7d45f700ac1c572
Matthew Hagemann <matt.hagemann@canonical.com>
2025-06-05 17:53:16 +0200

    feat: add required providers for recovery key replacement

--- packages/security_center/lib/disk_encryption/disk_encryption_providers.dart
+++ packages/security_center/lib/disk_encryption/disk_encryption_providers.dart
@@ -5,21 +5,23 @@ import 'package:ubuntu_service/ubuntu_service.dart';
 
 part 'disk_encryption_providers.freezed.dart';
 part 'disk_encryption_providers.g.dart';
-part 'disk_encryption_providers.freezed.dart';
 
 /// Dialog state for managing the replace recovery key flow.
 @freezed
 sealed class ReplaceRecoveryKeyDialogState with _$CheckRecoveryKeyDialogState {
   factory ReplaceRecoveryKeyDialogState.empty() =
       ReplaceRecoveryKeyDialogStateEmpty;
-  factory ReplaceRecoveryKeyDialogState.input(String keyToCheck) =
-      ReplaceRecoveryKeyDialogStateInput;
-  factory ReplaceRecoveryKeyDialogState.qr() =
-      ReplaceRecoveryKeyDialogStateQr;
-  factory ReplaceRecoveryKeyDialogState.result(bool valid) =
-      ReplaceRecoveryKeyDialogStateResult;
+  factory ReplaceRecoveryKeyDialogState.waitingForUser(bool acknowledged) =
+      ReplaceRecoveryKeyDialogStateWaitingForUser;
+  factory ReplaceRecoveryKeyDialogState.qr() = ReplaceRecoveryKeyDialogStateQr;
+  factory ReplaceRecoveryKeyDialogState.save() =
+      ReplaceRecoveryKeyDialogStateSave;
+  factory ReplaceRecoveryKeyDialogState.replaced() =
+      ReplaceRecoveryKeyDialogStateReplaced;
   factory ReplaceRecoveryKeyDialogState.loading() =
       ReplaceRecoveryKeyDialogStateLoading;
+  factory ReplaceRecoveryKeyDialogState.generating() =
+      ReplaceRecoveryKeyDialogStateGenerating;
   factory ReplaceRecoveryKeyDialogState.error(Exception e) =
       ReplaceRecoveryKeyDialogStateError;
 }
@@ -39,15 +41,50 @@ sealed class CheckRecoveryKeyDialogState with _$CheckRecoveryKeyDialogState {
       CheckRecoveryKeyDialogStateError;
 }
 
-@freezed
-class RecoveryKeyData with _$RecoveryKeyData {
-  const factory RecoveryKeyData() = _RecoveryKeyData;
+@riverpod
+class SystemContainersModel extends _$SystemContainersModel {
+  late final _service = getService<DiskEncryptionService>();
+
+  @override
+  Future<List<SystemDataContainer>> build() async {
+    final containers = await _service.enumerateKeySlots();
+    return containers;
+  }
 }
 
-// @riverpod
-// class ReplaceRecoveryKeyDialogModel extends _$ReplaceRecoveryKeyDialogModel {
-//    late final _service = getService<DiskEncryptionService>();
-// }
+@riverpod
+class ReplaceRecoveryKeyDialogModel extends _$ReplaceRecoveryKeyDialogModel {
+  late final _service = getService<DiskEncryptionService>();
+  RecoveryKeyDetails? generatedRecoveryKey;
+
+  @override
+  ReplaceRecoveryKeyDialogState build() =>
+      ReplaceRecoveryKeyDialogState.empty();
+
+  Future<void> generateRecoveryKey() async {
+    assert(state is ReplaceRecoveryKeyDialogStateGenerating);
+    try {
+      final result = await _service.generateRecoveryKey();
+      generatedRecoveryKey = result;
+      state = ReplaceRecoveryKeyDialogState.waitingForUser(false);
+    } on Exception catch (e) {
+      state = ReplaceRecoveryKeyDialogState.error(e);
+    }
+  }
+
+  Future<void> replaceRecoveryKey() async {
+    assert(state is ReplaceRecoveryKeyDialogStateWaitingForUser);
+    assert((state as ReplaceRecoveryKeyDialogStateWaitingForUser).acknowledged);
+    assert(generatedRecoveryKey != null);
+
+    try {
+      await _service.replaceRecoveryKey(generatedRecoveryKey!.keyId);
+      state = ReplaceRecoveryKeyDialogState.replaced();
+    } on Exception catch (e) {
+      state = ReplaceRecoveryKeyDialogState.error(e);
+    }
+  }
+}
 
 @riverpod
 class CheckRecoveryKeyDialogModel extends _$CheckRecoveryKeyDialogModel {
